7e73d5b1fe6d65b2637285d109cecf4b87a284dc
Cupcakes <110019749+Restond@users.noreply.github.com>
2025-03-14 19:43:19 +0800

    Update MT1002.java

--- Solutions/Java/MT1002.java
+++ Solutions/Java/MT1002.java
@@ -1,1 +1,10 @@
+import java.util.Scanner;
 
+class Main {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      int integer = input.nextInt();
+      System.out.print("You entered:" + integer);
+      input.close();
+   }
+}
