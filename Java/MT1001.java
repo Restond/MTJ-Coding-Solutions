04ee1c959e5160a7d96c69b1f95b9d25e320de50
Cupcakes <110019749+Restond@users.noreply.github.com>
2025-03-14 19:42:13 +0800

    Delete Java directory

--- Java/MT1001.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-class Main {
-   public static void main(String[] args) {
-      System.out.println("This is my first program!");
-      System.out.println("Coding is fun!");
-   }
-}
