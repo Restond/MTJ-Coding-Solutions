83ae0f1e4c64b408f57a37d467ae7e49649e0978
Cupcakes <110019749+Restond@users.noreply.github.com>
2025-03-14 19:42:36 +0800

    Update MT1001.java

--- Solutions/Java/MT1001.java
+++ Solutions/Java/MT1001.java
@@ -1,1 +1,8 @@
+import java.util.Scanner;
 
+class Main {
+   public static void main(String[] args) {
+      System.out.println("This is my first program!");
+      System.out.println("Coding is fun!");
+   }
+}
