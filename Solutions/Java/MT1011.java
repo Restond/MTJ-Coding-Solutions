456a814193d983e8af4d952df96d8f254c1ff4e8
Cupcakes <110019749+Restond@users.noreply.github.com>
2025-03-16 17:26:52 +0800

    Create MT1011.java

---
+++ Solutions/Java/MT1011.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.println("X " + (int)'X');
+      System.out.println((char)65 + " 65");
+      input.close();
+   }
+}
