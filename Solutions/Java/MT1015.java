b619c30e43a8833c05b1510c789470995a6c50e3
Cupcakes <110019749+Restond@users.noreply.github.com>
2025-03-17 13:27:51 +0800

    Update MT1015.java

--- Solutions/Java/MT1015.java
+++ Solutions/Java/MT1015.java
@@ -5,23 +5,10 @@ class Main {
       Scanner input = new Scanner(System.in);
       String[] pairs = input.nextLine().split(",");
 
-      int a = 0, c = 0;
-      char b = ' ', d = ' ';
-      
       for (String pair: pairs) {
         String[] keyValue = pair.split("=");
-        String key = keyValue[0];
-        String value = keyValue[1];
-
-        switch(key) {
-            case "a": a = Integer.parseInt(value); break;
-            case "b": b = value.charAt(0); break;
-            case "c": c = Integer.parseInt(value); break;
-            case "d": d = value.charAt(0); break;
-        }
+        System.out.print(keyValue[1] + " ");
       }
-
-      System.out.printf("%d %c %d %c", a, b, c, d);
       input.close();
    }
 }
