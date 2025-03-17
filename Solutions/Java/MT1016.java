3c0598d311557b4a73a1b38d2b57be4d0f239765
Cupcakes <110019749+Restond@users.noreply.github.com>
2025-03-17 13:31:48 +0800

    Create MT1016.java

---
+++ Solutions/Java/MT1016.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      int a = 455;
+      int b = -123;
+      int c = 987654;
+      System.out.printf("%-5d %5d\n", a, a);
+      System.out.printf("%-5d %5d\n", b, b);
+      System.out.printf("%-5d %5d\n", c, c);
+      input.close();
+   }
+}
