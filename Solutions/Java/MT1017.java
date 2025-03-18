30220788f268d6d0a8352ec0be0318c3ee882e26
Cupcakes <110019749+Restond@users.noreply.github.com>
2025-03-18 19:39:51 +0800

    Create MT1017.java

---
+++ Solutions/Java/MT1017.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+   public static void main(String[] args) {
+      double num1 = 3.1415926;
+      double num2 = 22.3456;
+      System.out.printf("%-14.6f %14.6f\n", num1, num1);
+      System.out.printf("%-14.6f %14.6f", num2, num2);
+   }
+}
