b52717089d34c2a43f88423c818aa6335dd453c6
Cupcakes <110019749+Restond@users.noreply.github.com>
2025-03-25 18:26:20 +0800

    Create MT1063.java

---
+++ Solutions/Java/MT1063.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      double a = input.nextDouble();
+      System.out.printf("%.6f", Math.pow(a, 3));
+      input.close();
+   }
+}
