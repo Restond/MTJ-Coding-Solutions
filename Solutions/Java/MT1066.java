8bbed44af48e59e5167eef6638cb26d9442479c0
Cupcakes <110019749+Restond@users.noreply.github.com>
2025-03-25 18:27:16 +0800

    Create MT1066.java

---
+++ Solutions/Java/MT1066.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      int N = input.nextInt();
+      System.out.print(N * 2);
+      input.close();
+   }
+}
